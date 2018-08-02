511777187c3c2ac779217a963bd14cc7090fe71f
ruanpeng <ruanpengwj43467@touna.cn>
2018-08-02 20:16:19 +0800

    编写生产者测试demo

--- src/main/java/com/feizi/MqProducerApplication.java
+++ src/main/java/com/feizi/MqProducerApplication.java
@@ -23,8 +23,7 @@ public class MqProducerApplication implements CommandLineRunner {
 	/**
 	 * MQ消息消费者topic定义
 	 */
-	private static final String MQ_TOPIC1 = "feizi_topic1";
-	private static final String MQ_TOPIC2 = "feizi_topic2";
+	private static final String MQ_TOPIC = "feizi_topic";
 
 	@Resource
 	private RocketMqSendTemplate rocketMqSendTemplate;
@@ -37,45 +36,31 @@ public class MqProducerApplication implements CommandLineRunner {
 	public void run(String... strings) throws Exception {
 		LOGGER.info("================start生产者发送MQ消息");
 
-		//first
-		MessageData<User> messageData1 = new MessageData<>();
-		messageData1.setUuid(UUID.randomUUID().toString());
-		messageData1.setTimestamp(System.currentTimeMillis());
-		User user1 = new User(1, "feizi", 25);
-		messageData1.setData(user1);
-
-		//second
-		MessageData<User> messageData2 = new MessageData<>();
-		messageData2.setUuid(UUID.randomUUID().toString());
-		messageData2.setTimestamp(System.currentTimeMillis());
-		User user2 = new User(2, "hello", 26);
-		messageData2.setData(user2);
-
+		/*############################first###############################*/
 		//封装MQ消息（first）
-		Message message1 = MessageUtils.wrapMessage(MQ_TOPIC1, messageData1);
+		Message message1 = MessageUtils.buildMessage(MQ_TOPIC, "tagA", "key001", new User(1, "feizi", 25));
+		sendMessage(message1);
 
-		//发送MQ消息
-		SendResult sendResult1 = rocketMqSendTemplate.syncSend(message1);
-		if(null != sendResult1){
-			/**
-			 * TODO 发送结果，发送完毕执行业务操作
-			 */
-			LOGGER.info("================sendResult1: " + sendResult1);
-		}
-
-		/*##################################################################*/
-
+		/*############################second###############################*/
 		//封装MQ消息（second）
-		Message message2 = MessageUtils.wrapMessage(MQ_TOPIC2, messageData2);
+		Message message2 = MessageUtils.buildMessage(MQ_TOPIC, "tagB", "key002", new User(2, "hello", 26));
+		sendMessage(message2);
 
-		//发送MQ消息, 如果失败，则尝试发送3次
-		SendResult sendResult2 = rocketMqSendTemplate.syncSend(message2, 3);
-		if(null != sendResult2){
+		LOGGER.info("================end生产者发送MQ消息");
+	}
+
+	/**
+	 * 发送MQ消息
+	 * @param message MQ消息
+	 */
+	private void sendMessage(Message message){
+		/* 发送MQ消息, 如果失败，则尝试发送3次 */
+		SendResult sendResult = rocketMqSendTemplate.syncSend(message, 3);
+		if(null != sendResult){
 			/**
 			 * TODO 发送结果，发送完毕执行业务操作
 			 */
-			LOGGER.info("================sendResult2: " + sendResult2);
+			LOGGER.info("================sendResult: " + sendResult);
 		}
-		LOGGER.info("================end生产者发送MQ消息");
 	}
 }
